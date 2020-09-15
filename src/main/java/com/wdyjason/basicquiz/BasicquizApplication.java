b80e1bbebce2b83b2d539e5b20bb83a013e804f8
wdyjason <ddd123321456555@qq.com>
2020-09-15 10:54:51 +0800

    init project

---
+++ src/main/java/com/wdyjason/basicquiz/BasicquizApplication.java
@@ -0,0 +1,13 @@
+package com.wdyjason.basicquiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicquizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicquizApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wdyjason/basicquiz/BasicquizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wdyjason.basicquiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicquizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
