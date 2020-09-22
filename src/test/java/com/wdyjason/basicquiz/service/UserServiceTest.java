ce14cf65bf8504abd86ebb93693f7a00442f0cbd
wdyjason <ddd123321456555@qq.com>
2020-09-22 18:36:45 +0800

    refactoring service unit test

--- src/test/java/com/wdyjason/basicquiz/service/UserServiceTest.java
+++ src/test/java/com/wdyjason/basicquiz/service/UserServiceTest.java
@@ -7,6 +7,7 @@ import com.wdyjason.basicquiz.exception.UserNotFoundException;
 import com.wdyjason.basicquiz.repository.EducationRepository;
 import com.wdyjason.basicquiz.repository.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -38,128 +39,156 @@ class UserServiceTest {
         MockitoAnnotations.initMocks(this);
     }
 
-    @Test
-    public void shouldCreateUserSuccessfully() {
-        User receivedUser = User.builder()
-                .name("test")
-                .avatar("url")
-                .age(18L)
-                .description("des")
-                .build();
+    @Nested
+    class UserAbout {
 
-        User returnedUser = User.builder()
-                .id(1L)
-                .name("test")
-                .age(18L)
-                .avatar("url")
-                .description("des")
-                .build();
+        @Nested
+        class happyPath {
 
-        when(userRepository.save(fromUser(receivedUser))).thenReturn(fromUser(returnedUser));
+            @Test
+            public void should_create_user_successfully() {
+                User receivedUser = User.builder()
+                        .name("test")
+                        .avatar("url")
+                        .age(18L)
+                        .description("des")
+                        .build();
 
-        Long result = userService.saveUser(receivedUser);
+                User returnedUser = User.builder()
+                        .id(1L)
+                        .name("test")
+                        .age(18L)
+                        .avatar("url")
+                        .description("des")
+                        .build();
 
-        assertEquals(result, 1L);
+                when(userRepository.save(fromUser(receivedUser))).thenReturn(fromUser(returnedUser));
+
+                Long result = userService.saveUser(receivedUser);
+
+                assertEquals(result, 1L);
+            }
+
+            @Test
+            public void should_get_a_user_successfully() throws UserNotFoundException {
+                Long userId = 1L;
+
+                User returnedUser = User.builder()
+                        .id(1L)
+                        .age(18L)
+                        .name("test")
+                        .avatar("url")
+                        .description("des")
+                        .build();
+
+                User expectedUser = User.builder()
+                        .id(userId)
+                        .age(18L)
+                        .name("test")
+                        .avatar("url")
+                        .description("des")
+                        .build();
+                when(userRepository.findOneById(userId)).thenReturn(Optional.of(fromUser(returnedUser)));
+
+                User result = userService.findOneUser(userId);
+
+                assertEquals(expectedUser, result);
+            }
+        }
+
+        @Nested
+        class sadPath {
+
+            @Test
+            public void should_get_a_user_failed_when_user_not_found() {
+                when(userRepository.findOneById(1L)).thenReturn(Optional.empty());
+                assertThrows(UserNotFoundException.class, () ->userService.findOneUser(1L));
+
+            }
+        }
     }
 
-    @Test
-    public void shouldGetAUserSuccessfully() throws UserNotFoundException {
-        Long userId = 1L;
+    @Nested
+    class EducationAbout {
 
-        User returnedUser = User.builder()
-                .id(1L)
-                .age(18L)
-                .name("test")
-                .avatar("url")
-                .description("des")
-                .build();
+        @Nested
+        class happyPath {
 
-        User expectedUser = User.builder()
-                .id(userId)
-                .age(18L)
-                .name("test")
-                .avatar("url")
-                .description("des")
-                .build();
-        when(userRepository.findOneById(userId)).thenReturn(Optional.of(fromUser(returnedUser)));
+            @Test
+            public void shouldCreateEducationSuccess() throws UserNotFoundException {
+                Education receivedEdu = Education.builder()
+                        .year(2020L)
+                        .title("title")
+                        .description("des")
+                        .build();
 
-        User result = userService.findOneUser(userId);
+                Education toSavedEdu = Education.builder()
+                        .userId(1L)
+                        .year(2020L)
+                        .title("title")
+                        .description("des")
+                        .build();
 
-        assertEquals(expectedUser, result);
-    }
+                Education returnedEdu = Education.builder()
+                        .id(1L)
+                        .userId(1L)
+                        .year(2020L)
+                        .title("title")
+                        .description("des")
+                        .build();
 
-    @Test
-    public void shouldGetAUserFailure() {
-        when(userRepository.findOneById(1L)).thenReturn(Optional.empty());
+                Education expectEdu = Education.builder()
+                        .id(1L)
+                        .userId(1L)
+                        .year(2020L)
+                        .title("title")
+                        .description("des")
+                        .build();
+                UserEntity constraintUser = UserEntity.builder().id(1L).build();
+                when(userRepository.findOneById(1L)).thenReturn(Optional.of(constraintUser));
+                when(educationRepository.save(fromEdu(toSavedEdu, constraintUser))).thenReturn(fromEdu(returnedEdu, constraintUser));
 
-        assertThrows(UserNotFoundException.class, () ->userService.findOneUser(1L));
+                Education result = userService.saveEducation(1L, receivedEdu);
+                assertEquals(result, expectEdu);
+            }
 
-    }
+            @Test
+            public void shouldGetEducationsSuccess() {
+                Education returnedEdu = Education.builder()
+                        .id(1L)
+                        .userId(1L)
+                        .year(2020L)
+                        .title("title")
+                        .description("des")
+                        .build();
 
-    // GTB: - 运行全部测试时，这个测试是失败的
-    @Test
-    public void shouldCreateEducationSuccess() throws UserNotFoundException {
-        Education receivedEdu = Education.builder()
-                .year(2020L)
-                .title("title")
-                .description("des")
-                .build();
+                Education expectEdu = Education.builder()
+                        .id(1L)
+                        .userId(1L)
+                        .year(2020L)
+                        .title("title")
+                        .description("des")
+                        .build();
 
-        Education toSavedEdu = Education.builder()
-                .userId(1L)
-                .year(2020L)
-                .title("title")
-                .description("des")
-                .build();
+                UserEntity constraintUser = UserEntity.builder().id(1L).build();
 
-        Education returnedEdu = Education.builder()
-                .id(1L)
-                .userId(1L)
-                .year(2020L)
-                .title("title")
-                .description("des")
-                .build();
+                when(educationRepository.findByUserId(1L)).thenReturn(Arrays.asList(fromEdu(returnedEdu, constraintUser)));
 
-        Education expectEdu = Education.builder()
-                .id(1L)
-                .userId(1L)
-                .year(2020L)
-                .title("title")
-                .description("des")
-                .build();
-        UserEntity constraintUser = UserEntity.builder().id(1L).build();
-        when(userRepository.findOneById(1L)).thenReturn(Optional.of(constraintUser));
-        when(educationRepository.save(fromEdu(toSavedEdu, constraintUser))).thenReturn(fromEdu(returnedEdu, constraintUser));
+                List<Education> result = userService.findUserEducations(1L);
 
-        Education result = userService.saveEducation(1L, receivedEdu);
-        assertEquals(result, expectEdu);
-    }
+                assertEquals(Arrays.asList(expectEdu), result);
 
-    @Test
-    public void shouldGetEducationsSuccess() {
-        Education returnedEdu = Education.builder()
-                .id(1L)
-                .userId(1L)
-                .year(2020L)
-                .title("title")
-                .description("des")
-                .build();
+            }
+        }
 
-        Education expectEdu = Education.builder()
-                .id(1L)
-                .userId(1L)
-                .year(2020L)
-                .title("title")
-                .description("des")
-                .build();
+        @Nested
+        class sadPath {
 
-        UserEntity constraintUser = UserEntity.builder().id(1L).build();
-
-        when(educationRepository.findByUserId(1L)).thenReturn(Arrays.asList(fromEdu(returnedEdu, constraintUser)));
-
-        List<Education> result = userService.findUserEducations(1L);
-
-        assertEquals(Arrays.asList(expectEdu), result);
-
+            @Test
+            public void should_save_educations_failed_when_user_not_found() {
+                when(userRepository.findOneById(1L)).thenReturn(Optional.empty());
+                assertThrows(UserNotFoundException.class, () ->userService.saveEducation(1L, new Education()));
+            }
+        }
     }
 }
